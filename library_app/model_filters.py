911e59b9083816e31e8fc36a3026b762decd7dd8
Shiryaev Maxim <maksim155555@gmail.com>
2020-02-19 13:25:11 +0200

    filtration edited

--- library_app/model_filters.py
+++ library_app/model_filters.py
@@ -4,6 +4,8 @@ from .models import Book
 
 
 class BookFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(lookup_expr='iexact')
+
     class Meta:
         model = Book
         fields = ("title", "year_made", "publisher_company", "authors")
