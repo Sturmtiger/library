6eb5364a1a33473c458610a0eb7036d5b45bb945
Shiryaev Maxim <maksim155555@gmail.com>
2020-02-21 14:15:54 +0200

    book ordering fixed 1

--- library_app/model_filters.py
+++ library_app/model_filters.py
@@ -1,17 +1,44 @@
 import django_filters
-
+from django_filters.constants import EMPTY_VALUES
+from django.db.models import FilteredRelation, Q
 from .models import Book
+
+
+class CustomOrderingFilter(django_filters.OrderingFilter):
+    """
+    Added specific ordering by author for Book model.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.extra['choices'] += [
+            ('main__author__pseudonym', 'Author'),
+            ('-main__author__pseudonym', 'Author (descending)'),
+        ]
+
+    def filter(self, qs, value):
+        if value in EMPTY_VALUES:
+            return qs
+        if any(v in ['main__author__pseudonym',
+                     '-main__author__pseudonym'] for v in value):
+            ordering = [self.get_ordering_value(param) for param in value]
+            return qs.annotate(
+                main=FilteredRelation('bookauthorspriority',
+                                      condition=Q(
+                                          bookauthorspriority__main=True))
+            ).order_by(*ordering)
+
+        return super().filter(qs, value)
 
 
 class BookFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(lookup_expr="iexact")
 
-    o = django_filters.OrderingFilter(
+    o = CustomOrderingFilter(
         fields=(
             ("title", "title"),
             ("year_made", "year_made"),
             ("publisher_company", "publisher_company"),
-            ("authors", "authors"),
         )
     )
 
