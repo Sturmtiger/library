e409e8b54a8ba70a35574fb07c04646761bae992
Shiryaev Maxim <maksim155555@gmail.com>
2020-03-06 14:13:30 +0200

    set password view fixed

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,41 +1,51 @@
-from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('admin_panel/', views.AdminPanelView.as_view(), name='admin_panel'),
-    path('profile/', views.UserProfileView.as_view(), name='profile'),
-    path('update_user/', views.UpdateUserView.as_view(), name='update_user'),
-    path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', views.SignUpView.as_view(), name='signup'),
-
-    path('password_change/',
-         auth_views.PasswordChangeView.as_view(
-             template_name='users_app/password_change/form.html',
-         ),
-         name='password_change'),
-    path('password_change/done/',
-         auth_views.PasswordChangeDoneView.as_view(
-             template_name='users_app/password_change/done.html',
-         ),
-         name='password_change_done'),
-
-    path('password_reset/',
-         views.CustomPasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/',
-         auth_views.PasswordResetDoneView.as_view(
-             template_name='users_app/password_reset/done.html',
-         ),
-         name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(
-             template_name='users_app/password_reset/confirm.html',
-         ),
-         name='password_reset_confirm'),
-    path('reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(
-             template_name='users_app/password_reset/complete.html',
-         ),
-         name='password_reset_complete'),
+    path("admin_panel/", views.AdminPanelView.as_view(), name="admin_panel"),
+    path("profile/", views.UserProfileView.as_view(), name="profile"),
+    path("update_user/", views.UpdateUserView.as_view(), name="update_user"),
+    path("login/", views.CustomLoginView.as_view(), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
+    path(
+        "password_change/",
+        auth_views.PasswordChangeView.as_view(
+            template_name="users_app/password_change/form.html"
+        ),
+        name="password_change",
+    ),
+    path(
+        "password_change/done/",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="users_app/password_change/done.html"
+        ),
+        name="password_change_done",
+    ),
+    path(
+        "password_reset/",
+        views.CustomPasswordResetView.as_view(),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="users_app/password_reset/done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        views.CustomPasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="users_app/password_reset/complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
--- users_app/views.py
+++ users_app/views.py
@@ -1,29 +1,30 @@
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.tokens import default_token_generator
-from django.contrib.auth.views import LoginView, PasswordResetView
+from django.contrib.auth.views import (LoginView, PasswordResetConfirmView,
+                                       PasswordResetView)
 from django.contrib.sites.shortcuts import get_current_site
-from django.template.loader import render_to_string
 from django.core.mail import send_mail
-from django.shortcuts import render, redirect
+from django.db import transaction
+from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from django.views import View
-from django.contrib.auth import authenticate, login
-from django.contrib import messages
-from django.conf import settings
-from django.db import transaction
 
-from .forms import (SignUpForm, CreatePublisherUserForm,
-                    UpdateUserForm, UpdateProfileForm,
-                    AssignPublisherCompanyToUserPublisherForm)
-from .models import Profile
 from .custom_mixins import UserIsAdminMixin, UserIsNotLoggedIn
+from .forms import (AssignPublisherCompanyToUserPublisherForm,
+                    CreatePublisherUserForm, SignUpForm, UpdateProfileForm,
+                    UpdateUserForm)
+from .models import Profile
 
 
 class SignUpView(UserIsNotLoggedIn, View):
     def get(self, request):
         form = SignUpForm()
-        return render(request, 'users_app/signup.html', {'form': form})
+        return render(request, "users_app/signup.html", {"form": form})
 
     @transaction.atomic
     def post(self, request):
@@ -31,17 +32,17 @@ class SignUpView(UserIsNotLoggedIn, View):
         if form.is_valid():
             user = form.save()
             user.refresh_from_db()
-            user.profile.patronymic = form.cleaned_data.get('patronymic')
-            user.profile.birthday = form.cleaned_data.get('birthday')
+            user.profile.patronymic = form.cleaned_data.get("patronymic")
+            user.profile.birthday = form.cleaned_data.get("birthday")
             user.profile.type = Profile.READER
             user.save()
 
-            password = form.cleaned_data.get('password1')
+            password = form.cleaned_data.get("password1")
             user = authenticate(username=user.username, password=password)
             login(request, user)
 
             return redirect(settings.LOGIN_REDIRECT_URL)
-        return render(request, 'users_app/signup.html', {'form': form})
+        return render(request, "users_app/signup.html", {"form": form})
 
 
 class AdminPanelView(UserIsAdminMixin, View):
@@ -50,17 +51,17 @@ class AdminPanelView(UserIsAdminMixin, View):
         assign_publisher_form = AssignPublisherCompanyToUserPublisherForm()
 
         context = {
-            'create_publisher_form': create_publisher_form,
-            'assign_publisher_form': assign_publisher_form
+            "create_publisher_form": create_publisher_form,
+            "assign_publisher_form": assign_publisher_form,
         }
 
-        return render(request, 'users_app/admin_panel.html', context=context)
+        return render(request, "users_app/admin_panel.html", context=context)
 
     def post(self, request):
         create_publisher_form = CreatePublisherUserForm()
         assign_publisher_form = AssignPublisherCompanyToUserPublisherForm()
 
-        if 'invite_publisher' in request.POST:
+        if "invite_publisher" in request.POST:
             create_publisher_form = CreatePublisherUserForm(request.POST)
             if create_publisher_form.is_valid():
                 user = create_publisher_form.save()
@@ -71,20 +72,18 @@ class AdminPanelView(UserIsAdminMixin, View):
                 domain = current_site.domain
 
                 mail_context = {
-                    'domain': domain,
-                    'site_name': site_name,
-                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                    'user': user,
-                    'token': default_token_generator.make_token(user),
-                    'protocol': 'https' if self.request.is_secure()
-                    else 'http',
+                    "domain": domain,
+                    "site_name": site_name,
+                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                    "user": user,
+                    "token": default_token_generator.make_token(user),
+                    "protocol": "https" if self.request.is_secure() else "http",
                 }
 
-                subject = render_to_string(
-                    'users_app/password_set/subject.txt')
+                subject = render_to_string("users_app/password_set/subject.txt")
                 message = render_to_string(
-                    'users_app/password_set/email.html',
-                    context=mail_context)
+                    "users_app/password_set/email.html", context=mail_context
+                )
 
                 send_mail(
                     subject=subject,
@@ -92,26 +91,30 @@ class AdminPanelView(UserIsAdminMixin, View):
                     from_email=None,
                     recipient_list=[user.email],
                 )
-                messages.success(request,
-                                 'Publisher-user has been created '
-                                 'and message has been sent.')
+                messages.success(
+                    request,
+                    "Publisher-user has been created " "and message has been sent.",
+                )
 
-        if 'assign_company' in request.POST:
+        if "assign_company" in request.POST:
             assign_publisher_form = AssignPublisherCompanyToUserPublisherForm(
-                request.POST)
+                request.POST
+            )
             if assign_publisher_form.is_valid():
                 publisher_user = assign_publisher_form.save()
                 publisher_company = publisher_user.profile.publisher_company
-                messages.success(request,
-                                 f'Publisher-company "{publisher_company}" '
-                                 f'has been assigned to "{publisher_user}".')
+                messages.success(
+                    request,
+                    f'Publisher-company "{publisher_company}" '
+                    f'has been assigned to "{publisher_user}".',
+                )
 
         context = {
-            'create_publisher_form': create_publisher_form,
-            'assign_publisher_form': assign_publisher_form
+            "create_publisher_form": create_publisher_form,
+            "assign_publisher_form": assign_publisher_form,
         }
 
-        return render(request, 'users_app/admin_panel.html', context=context)
+        return render(request, "users_app/admin_panel.html", context=context)
 
 
 class UserProfileView(LoginRequiredMixin, View):
@@ -119,42 +122,39 @@ class UserProfileView(LoginRequiredMixin, View):
         books = None
 
         user = request.user
-        if (user.profile.type == Profile.PUBLISHER and
-                user.profile.publisher_company):
+        if user.profile.type == Profile.PUBLISHER and user.profile.publisher_company:
             books = user.profile.publisher_company.books.all()
 
-        return render(request, 'users_app/profile.html', {'books': books})
+        return render(request, "users_app/profile.html", {"books": books})
 
 
 class UpdateUserView(View):
     def get(self, request):
         user_form = UpdateUserForm(instance=request.user)
         profile_form = UpdateProfileForm(instance=request.user.profile)
-        next_url = self.request.GET.get('next', '/')
-        return render(request, 'users_app/update_user.html', {
-            'user_form': user_form,
-            'profile_form': profile_form,
-            'next': next_url,
-        })
+        next_url = self.request.GET.get("next", "/")
+        return render(
+            request,
+            "users_app/update_user.html",
+            {"user_form": user_form, "profile_form": profile_form, "next": next_url},
+        )
 
     @transaction.atomic
     def post(self, request):
         user_form = UpdateUserForm(request.POST, instance=request.user)
-        profile_form = UpdateProfileForm(request.POST,
-                                         instance=request.user.profile)
-        next_url = self.request.GET.get('next', '/')
+        profile_form = UpdateProfileForm(request.POST, instance=request.user.profile)
+        next_url = self.request.GET.get("next", "/")
 
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.success(request,
-                             'Your data has been updated successfully!')
+            messages.success(request, "Your data has been updated successfully!")
             return redirect(next_url)
-        return render(request, 'users_app/update_user.html', {
-            'user_form': user_form,
-            'profile_form': profile_form,
-            'next': next_url,
-        })
+        return render(
+            request,
+            "users_app/update_user.html",
+            {"user_form": user_form, "profile_form": profile_form, "next": next_url},
+        )
 
 
 class CustomLoginView(UserIsNotLoggedIn, LoginView):
@@ -162,5 +162,14 @@ class CustomLoginView(UserIsNotLoggedIn, LoginView):
 
 
 class CustomPasswordResetView(UserIsNotLoggedIn, PasswordResetView):
-    template_name = 'users_app/password_reset/form.html'
-    subject_template_name = 'users_app/password_reset/subject.txt'
+    template_name = "users_app/password_reset/form.html"
+    subject_template_name = "users_app/password_reset/subject.txt"
+
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    template_name = "users_app/password_reset/confirm.html"
+
+    def dispatch(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            logout(self.request)
+        return super().dispatch(*args, **kwargs)
